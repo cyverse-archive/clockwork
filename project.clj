81da09077ca3dce15753359a43b72184f643a027
John Wregglesworth <wregglej@iplantcollaborative.org>
2013-06-27 10:27:32 -0700

    CORE-4288: Added iplant-clavin as a dependency.

--- project.clj
+++ project.clj
@@ -28,7 +28,7 @@
   :main clockwork.core
   :iplant-rpm {:summary "Scheduled jobs for the iPlant Discovery Environment"
                :provides "clockwork"
-               :dependencies ["iplant-service-config >= 0.1.0-5"]
+               :dependencies ["iplant-service-config >= 0.1.0-5" "iplant-clavin"]
                :config-files ["log4j.properties"]
                :config-path "resources/main"}
   :uberjar-exclusions [#"BCKEY.SF"]
