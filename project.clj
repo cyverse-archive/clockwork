fac98cdeca4fd28141361c1ea1b6acdb91ee11ae
John Wregglesworth <wregglej@iplantcollaborative.org>
2013-10-29 08:05:21 -0700

    CORE-4922: Added openjdk as a dependency.

--- project.clj
+++ project.clj
@@ -28,7 +28,7 @@
   :main clockwork.core
   :iplant-rpm {:summary "Scheduled jobs for the iPlant Discovery Environment"
                :provides "clockwork"
-               :dependencies ["iplant-service-config >= 0.1.0-5" "iplant-clavin"]
+               :dependencies ["iplant-service-config >= 0.1.0-5" "iplant-clavin" "java-1.7.0-openjdk"]
                :config-files ["log4j.properties"]
                :config-path "resources/main"}
   :uberjar-exclusions [#"BCKEY.SF"]
